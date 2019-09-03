b4548133efac695aa702a5f790104a80bb5ceb92
ivallesp <ivanvallesperez@gmail.com>
2019-09-03 23:18:57 +0200

    Add documentation for lattice and util modules

--- somnium/util.py
+++ somnium/util.py
@@ -1,4 +1,14 @@
 def batching(list_of_iterables, n=1, infinite=False, return_incomplete_batches=False):
+    """
+    Takes one or more iterable inputs as argument and splits it in equally-sized chunks.
+    :param list_of_iterables: list of equally-sized iterables (list)
+    :param n: chunk size (int)
+    :param infinite: when set to True, the data is splited in chunks in a cycling way so that the iterator never ends
+    (bool)
+    :param return_incomplete_batches: if True, when n is not multiple of the length of the iterables, the last chunk
+    is returned. Otherwise it is skipped (bool)
+    :return: generator of iterables (list)
+    """
     list_of_iterables = [list_of_iterables] if type(list_of_iterables) is not list else list_of_iterables
     assert (len({len(it) for it in list_of_iterables}) == 1)
     length = len(list_of_iterables[0])
