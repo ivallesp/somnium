9c8c65dc30add9d740840aff6b1a3d539f765b77
ivallesp <ivanvallesperez@gmail.com>
2019-09-05 11:21:21 +0200

    Fix neighborhood checker for hexagonal lattice. Add tests for lattice. Add euclidean function to test.util

--- somnium/tests/util.py
+++ somnium/tests/util.py
@@ -1,3 +1,6 @@
+import math
+
+
 def strictly_increasing(L):
     return all(x<y for x, y in zip(L, L[1:]))
 
@@ -12,3 +15,8 @@ def non_decreasing(L):
 
 def monotonic(L):
     return non_increasing(L) or non_decreasing(L)
+
+
+def euclidean_distance(x, y):
+    squares = [(xi - yi)**2 for (xi, yi) in zip(x, y)]
+    return math.sqrt(sum(squares))
